55c74fa7bafdfbe0482bd3c5436b94af6f094de6
Pat George <pat.george@gmail.com>
2010-05-31 00:02:05 -0400

    Lots of style change and resized the thumbnails.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100#" }
 
   named_scope :by_category,  lambda { |*args|
     { :conditions => ["category = ?", args.first], :order => 'photo_file_name ASC' }
