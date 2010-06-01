f9073a5be038651e236639bfcf81e24c0a97f709
Pat George <pat.george@gmail.com>
2010-06-01 12:03:19 -0400

    More style changes.  Implemented flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def flash_notices
+    [:notice, :error].collect {|type| content_tag('div', flash[type], :class => type, :id => "flash") if flash[type] }
+  end
 end
--- config/initializers/action_view_settings.rb
+++ config/initializers/action_view_settings.rb
@@ -1,1 +1,3 @@
 ActionView::Base.default_form_builder = LabelingFormBuilder
+
+ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| "<span class=\"fieldWithErrors\">#{html_tag}</span>" }
