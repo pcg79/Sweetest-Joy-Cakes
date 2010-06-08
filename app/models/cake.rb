46d74e08edc032d0abc4b6d85cdf998d1f2e42cc
Pat George <pat.george@gmail.com>
2010-06-08 14:07:35 -0400

    Code to rotate images.  Doesn't seem to work, though.

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,8 +1,32 @@
 class Cake < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100#" }
+  has_attached_file :photo,
+                    :styles => { :medium => "500x500>", :thumb => "100x100#" },
+                    :processors => [:rotator]
 
-  scope :by_category,  lambda { |*args|
-    { :conditions => ["category = ?", args.first], :order => 'photo_file_name ASC' }
-  }
+  scope :by_category,  lambda { |*args| where("category = ?", args.first).order('photo_file_name ASC') }
 
+  attr_accessor :rotation, :rotate
+
+  before_create :set_defaults
+
+
+  def rotate!(degrees = 90)
+    self.rotation += degrees
+    self.rotation -= 360 if self.rotation >= 360
+    self.rotation += 360 if self.rotation <= -360
+
+    self.rotate = true
+    self.photo.reprocess!
+    self.save
+  end
+
+  def rotating?
+    !self.rotation.nil? and self.rotate
+  end
+
+  private
+
+  def set_defaults
+    self.rotation ||= 0
+  end
 end
