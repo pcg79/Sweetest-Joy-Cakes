aaae167dce147aa807dedc30c5ba19bde1a3bf47
Pat George <pat.george@gmail.com>
2010-06-08 14:07:53 -0400

    Ability to specify more than one category to pull from.

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -19,21 +19,25 @@ class CakesController < ApplicationController
   end
 
   def birthdays
-    render_pictures 'birthdays', 'Birthday Cakes'
+    render_pictures 'Birthday Cakes', 'birthdays',
   end
 
   def weddings
-    render_pictures 'weddings', 'Wedding Cakes'
+    render_pictures 'Wedding Cakes', 'weddings'
   end
 
   def others
-    render_pictures 'other', 'Cakes for Other Special Occasions'
+    render_pictures 'Cakes for Other Special Occasions', 'other', 'practice'
   end
 
   private
 
-  def render_pictures(category, title=nil)
-    @pictures = Cake.by_category(category) / PicsPerRow
+  def render_pictures(title, *categories)
+    @pictures = Cake.order('photo_file_name ASC')
+    categories.each do |category|
+      @pictures.where('category = ', category)
+    end
+    @pictures = @pictures / PicsPerRow
     @title = title
     render :action => 'index'
   end
