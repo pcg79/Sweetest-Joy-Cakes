ac7bd92f116a251070ef7a5b42ad100cdf602575
Pat George <pat.george@gmail.com>
2011-04-06 20:47:22 -0400

    Upgraded to Rails 3.0.6.

---
+++ bin/start_pgsql.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo su postgres -c '/opt/local/lib/postgresql83/bin/postgres -D /opt/local/var/db/postgresql83/defaultdb &'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,9 @@ JoysCakes::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
